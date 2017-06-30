33bfd3b508f26b1ebef1fea5d05cc769c84ffc4e
jake <omonoid@uw.edu>
2017-06-29 22:46:00 -0700

    added plots
    Former-commit-id: 7cc356728a6991e4800c25e45a83394699b157ea

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 from flask import Flask, render_template
 
@@ -8,15 +7,29 @@ from scraper.constants import ORDER
 
 app = Flask(__name__)
 
-db = MongoDB(host=os.environ['DB_PORT_27017_TCP_ADDR'], port=27017)  # for inside docker
-# db = MongoDB(host='localhost', port=27017)  # debug
+# db = MongoDB(host=os.environ['DB_PORT_27017_TCP_ADDR'], port=27017)  # for inside docker
+db = MongoDB(host='localhost', port=27017)  # debug
+
+def create_plots(data):
+    # iterate of array of raw data and make svg plot
+    plots = []
+    for i, row in enumerate(data):
+        id = 'plot%d' % i
+        fcn = "plot('%s', %s)" % (id, str(row))
+        svg_str = '<svg id="%s" onload="%s"/>' % (id, fcn)
+        plots.append(svg_str)
+    return plots
+
+
 
 
 def df_to_html(df):
     pd.set_option('display.max_colwidth', -1)
-    df = df.drop(['week data'], axis=1)
-    cols = list(df)
+    df['week data'] = create_plots(df['week data'].values)
+    # df = df.drop(['week data'], axis=1)
+    cols = list(df)  # reorder
     cols.insert(0, cols.pop(cols.index('img')))
+    cols.insert(9, cols.pop(cols.index('week data')))
     df = df[cols]
     return df.to_html(escape=False).replace('class="dataframe"', 'class="sortable"')
 
